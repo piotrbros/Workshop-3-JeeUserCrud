6a385af5df07f089afa205a5e431f33b97e9c129
piotrbros <pibro@visa.com>
2026-07-23 16:43:40 +0200

    Setting up project.

--- src/main/java/pl/coderslab/DbUtil.java
+++ src/main/java/pl/coderslab/DbUtil.java
@@ -1,16 +1,26 @@
 package pl.coderslab;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbUtil {
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/workshop2?useSSL=false&characterEncoding=utf8";
-    private static final String DB_USER = "root";
-    private static final String DB_PASS = SqlPassword.get();
-
+    private static DataSource dataSource;
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+        return getInstance().getConnection();   }
+    private static DataSource getInstance() {
+        if (dataSource == null) {
+            try {
+                Context initContext = new InitialContext();
+                Context envContext = (Context)initContext.lookup("java:/comp/env");
+                dataSource = (DataSource)envContext.lookup("jdbc/users");
+            } catch (NamingException e) { e.printStackTrace(); }
+        }
+        return dataSource;
     }
 }
 
