b552900bbbd8cd337d4b618c1c22172bf6e0c3d9
piotrbros <pibro@visa.com>
2026-07-23 17:03:48 +0200

    Setting up project.

--- src/main/java/pl/coderslab/utils/DbUtil.java
+++ src/main/java/pl/coderslab/utils/DbUtil.java
@@ -1,11 +1,10 @@
-package pl.coderslab;
+package pl.coderslab.utils;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbUtil {
